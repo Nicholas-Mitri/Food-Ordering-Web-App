01a58acaa1c50ef6f91ae9da16a8b53edeb5c86c
Nicholas Mitri <nicholas.g.mitri@gmail.com>
2025-04-20 21:50:08 -0400

    Implement menu management features in the food ordering app
    - Added `MenuItemDetail` admin interface for better management of menu items.
    - Created `urls.py` for routing to the home page and menu list view.
    - Developed `MenuList` and `MenuItemDetail` views to display menu items and their details.
    - Introduced `index.html` template for rendering the restaurant menu.
    - Updated `mysite/urls.py` to include the food ordering app's URLs.
    - Added a new `base.html` template for consistent layout across pages.

--- food_ordering/views.py
+++ food_ordering/views.py
@@ -1,3 +1,24 @@
+from re import template
 from django.shortcuts import render
+from django.views import generic
+from .models import MEAL_TYPE, Item
 
-# Create your views here.
+
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("date_created")
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+        print(context.keys())
+        return context
+
+
+class MenuItemDetail(generic.DetailView):
+    model = Item
+    template_name = "menu_item_detail.html"
+
+
+class AddItem(generic.CreateView):
+    pass
